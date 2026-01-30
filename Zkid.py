c4d8c4cd0694d97a6f1f5872db99b6a99a924152
cgrokpp <abobspongeb2@gmail.com>
2026-01-30 13:08:08 +0200

    Zkid release

---
+++ Zkid.py
@@ -0,0 +1,29 @@
+import os
+
+os.system("clear")
+
+
+#Table of colors
+D = "\033[1;30m" # DARK
+R = "\033[1;31m" # RED
+G = "\033[1;32m" # GREEN
+Y = "\033[1;33m" # YELLOW
+B = "\033[1;34m" # BLUE
+V = "\033[1;35m" # VIOLET
+C = "\033[1;36m" # CYAN
+W = "\033[1;37m" # WHITE
+RE = "\033[0m"   # REMOVE
+# 1;30m, 1/0 = bold, 30-37 color, 90-97 light color.
+
+
+print(f"""
+{R}
+   ▟███████▙  █▟█  ▟█  ▟█  ▟██████▙
+   ▝▝▝▟██▛▘▘  ██▛▟█▛▘  ██  ██▝▝▝▟█▙
+     ▟██▛     █████▙   ██  ██   ▟█▛
+    ▟██▛      ██▛▝██▙  ██  ██   ▟█▛
+   ▟███████▙  ██   ██  ██  ▟██████▛
+   ▝▝▝▝▝▝▝▝▘  ▝▘   ▝▘  ▝▘  ▝▝▝▝▝▝▝
+{RE}          {V}TELEGRAM{RE}: {C}cgrokpp{RE}
+
+""")
